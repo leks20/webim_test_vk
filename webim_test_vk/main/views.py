d75613e03e2cbc208ae0d6ad1fa69a6e02533843
leks20 <leha-spb@yandex.ru>
2020-08-26 21:33:53 +0300

    Ready

---
+++ webim_test_vk/main/views.py
@@ -0,0 +1,86 @@
+import os
+import requests
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
+from dotenv import load_dotenv
+from json import JSONDecodeError
+from requests import exceptions
+
+
+load_dotenv()
+VK_TOKEN = os.getenv('VK_TOKEN')
+
+
+def home(request):
+    if not request.user.is_authenticated:
+        context = {}
+        return render(request, 'base.html', context)
+
+    BASE_URL = 'https://api.vk.com/method/'
+
+    method_profile = 'users.get'
+    method_friends = 'friends.get'
+
+    url_profile = os.path.join(BASE_URL, method_profile)
+    url_friends = os.path.join(BASE_URL, method_friends)
+
+    # получаем id, имя и фамилию пользователя
+    data = {
+        'access_token': VK_TOKEN,
+        'user_ids': request.user.username,
+        'v': 5.122,
+    }
+
+    try:
+        request_profile = requests.post(url_profile, params=data)
+        first_name = request_profile.json()['response'][0]['first_name']
+        last_name = request_profile.json()['response'][0]['last_name']
+        user_id = request_profile.json()['response'][0]['id']
+        profile = '{} {}'.format(first_name, last_name)
+    except KeyError:
+        print('Ошибка при получении данных из JSON-объекта')
+    except JSONDecodeError:
+        print('Не удалось получить ответ в формате JSON')
+    except exceptions.RequestException as e:
+        print(f'При попытке соединения возникла следующая ошибка: {e}')
+
+    # получаем имена и фамилии 5 друзей
+    data = {
+        'user_id': user_id,
+        'order': 'random',
+        'count': 5,
+        'fields': 'nickname',
+        'name_case': 'nom',
+        'access_token': VK_TOKEN,
+        'v': 5.122,
+    }
+    try:
+        request_friends = requests.post(url_friends, params=data)
+        friends = request_friends.json().get('response')['items']
+        one = '{} {}'.format(friends[0]['first_name'], friends[0]['last_name'])
+        two = '{} {}'.format(friends[1]['first_name'], friends[1]['last_name'])
+        three = '{} {}'.format(friends[2]['first_name'], friends[2]['last_name'])
+        four = '{} {}'.format(friends[3]['first_name'], friends[3]['last_name'])
+        five = '{} {}'.format(friends[4]['first_name'], friends[4]['last_name'])
+    except KeyError:
+        print('Ошибка при получении данных из JSON-объекта')
+    except JSONDecodeError:
+        print('Не удалось получить ответ в формате JSON')
+    except exceptions.RequestException as e:
+        print(f'При попытке соединения возникла следующая ошибка: {e}')
+
+    context = {
+        'profile': profile,
+        'one': one,
+        'two': two,
+        'three': three,
+        'four': four,
+        'five': five
+    }
+    return render(request, 'base.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
---
+++ webim_test_vk/webim_test_vk/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
+    path('', views.home, name='home'),
+    path('logout/', views.logout, name='logout'),
+]
